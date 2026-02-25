0ba921a54e4030ecefce184a631120a84ddab500
Masahiro Sakai <masahiro.sakai@gmail.com>
2026-02-26 01:26:12 +0900

    3.2 証明を楽にするコツ

--- lean/LeanBook/LeanBook/Logic/PropLogic.lean
+++ lean/LeanBook/LeanBook/Logic/PropLogic.lean
@@ -217,3 +217,73 @@ example (P Q : Prop) : ¬ (P ∨ Q) ↔ ¬ P ∧ ¬ Q := by
     | inr hq =>
         apply h.right
         exact hq
+
+-- 3.2 証明を楽にするコツ
+
+example (P : Prop) : ¬ ¬ ¬ P → ¬ P := by
+  intro hn3p hp
+
+  have hn2p : ¬ ¬ P := by
+    intro hnp
+    contradiction
+
+  contradiction
+
+example (P : Prop) : ¬ ¬ ¬ P → ¬ P := by
+  intro hn3p hp
+
+  have : ¬ ¬ P := by
+    intro hnp
+    contradiction
+
+  guard_hyp this : ¬ ¬ P
+
+  contradiction
+
+example (P : Prop) : ¬ ¬ (P ∨ ¬ P) := by
+  intro h
+
+  suffices hyp : ¬ P from by
+    have : P ∨ ¬ P := by
+      right
+      exact hyp
+
+    contradiction
+
+  guard_target =ₛ ¬ P
+
+  intro hp
+
+  have : P ∨ ¬ P := by
+    left
+    exact hp
+
+  contradiction
+
+example (P : Prop) : (P → True) ↔ True := by
+  -- exact?
+  exact imp_true_iff P
+
+example (P : Prop) : (True → P) ↔ P := by
+  -- exact?
+  exact true_imp_iff
+
+example (P Q : Prop) (h : ¬ P ↔ Q) : (P → False) ↔ Q := by
+  -- exact h
+  rw [show (P → False) ↔ ¬ P from by rfl]
+  rw [h]
+
+example (P : Prop) : ¬ (P ↔ ¬ P) := by
+  intro h
+
+  have : ¬ P := by
+    intro hp
+    apply h.mp
+    exact hp
+    exact hp
+
+  have : P := by
+    apply h.mpr
+    exact this
+
+  contradiction
