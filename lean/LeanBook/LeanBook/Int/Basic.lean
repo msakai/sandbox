420955ede3c7a3382a16148d2474ff2101e75c97
Masahiro Sakai <masahiro.sakai@gmail.com>
2026-07-19 10:39:08 +0900

    7.3.3 数値リテラルが使えるようにする and 7.3.4 練習問題

--- lean/LeanBook/LeanBook/Int/Basic.lean
+++ lean/LeanBook/LeanBook/Int/Basic.lean
@@ -58,3 +58,72 @@ abbrev MyInt := Quotient PreInt.sr
 notation:arg (priority := low) "⟦" a "⟧" => Quotient.mk _ a
 
 #check (⟦ (1, 3) ⟧ : MyInt)
+
+-- 7.3.3 数値リテラルが使えるようにする
+
+def MyInt.ofNat (n : Nat) : MyInt := ⟦ (MyNat.ofNat n, 0) ⟧
+
+instance {n : Nat} : OfNat MyInt n where
+  ofNat := MyInt.ofNat n
+
+#check (4 : MyInt)
+
+#check_failure (-4 : MyInt)
+
+def PreInt.neg : PreInt → MyInt
+  | (m, n) => ⟦ (n, m) ⟧
+
+@[notation_simp]
+theorem MyInt.sr_def (m n : PreInt) : m ≈ n ↔ m.1 + n.2 = m.2 + n.1 := by
+  rfl
+
+def MyInt.neg : MyInt → MyInt := Quotient.lift PreInt.neg <| by
+  intro (a₁, a₂) (b₁, b₂) hab
+
+  have : (a₂, a₁) ≈ (b₂, b₁) := by
+    notation_simp at *
+    simp_all
+
+  dsimp [PreInt.neg]
+  rw [Quotient.sound]
+  assumption
+
+instance : Neg MyInt where
+  neg := MyInt.neg
+
+@[simp]
+theorem MyInt.neg_def (x y : MyNat) : - ⟦ (x,y)⟧ = (⟦(y, x)⟧ : MyInt) := by
+  dsimp [Neg.neg, MyInt.neg, PreInt.neg]
+  rfl
+
+#check (-4 : MyInt)
+
+#check_failure -4
+
+-- テキストでこれを check しているのは何故?
+#check Setoid
+
+-- 7.3.4 練習問題 （回答は206 ページ）
+
+variable {α : Type} {r : α → α → Prop}
+
+private theorem Ex.symm (refl : ∀ x, r x x) (h : ∀ x y z, r x y → r y z → r z x)
+    : ∀ {x y : α}, r x y → r y x := by
+  intro x y rxy
+  exact h x y y rxy (refl y)
+
+private theorem Ex.equiv (refl : ∀ x, r x x)
+    (h : ∀ x y z, r x y → r y z → r z x) : Equivalence r := by
+  constructor
+
+  case refl =>
+    intro x
+    exact refl x
+
+  case symm =>
+    intro x y rxy
+    exact Ex.symm refl h rxy
+
+  case trans =>
+    intro x y z rxy ryz
+    exact Ex.symm refl h (h x y z rxy ryz)
